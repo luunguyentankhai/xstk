7e482b0e63cce7c0372051b5f1f28d2a701686f2
hnkk <100778377+luunguyentankhai@users.noreply.github.com>
2026-04-05 16:38:28 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from pathlib import Path
 from scripts.pulldata import Pulldata
+import pandas as pd
+import os
 
-Pulldata()
+#get data
+if Pulldata():
+    Pulldata()
+
+Curr_dir = Path(__file__).resolve().parent
+
+Data_dir = Curr_dir / "data" / "raw"
+
+df = pd.read_csv(os.path.join(Data_dir, "data.csv"))
+
+print(df.head(25))
