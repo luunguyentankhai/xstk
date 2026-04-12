9bc60ffbbeb038bae70dd98c9001307e0f424d8d
hnkk <100778377+luunguyentankhai@users.noreply.github.com>
2026-04-12 23:57:37 +0700

    update config

--- config.py
+++ config.py
@@ -5,5 +5,11 @@ Raw_Data_Path = Base_Path / "data" / "raw"
 Processed_Data_Path = Base_Path / "data" / "processed"
 Data_File = Raw_Data_Path / "data.csv"
 
+#Diagram
+Assets = Base_Path / "data" / "Assets"
+
+dependent_vars =  ["roughness", "tension_strenght", "elongation"]
+independent_vars = ["layer_height","wall_thickness","infill_density","nozzle_temperature","bed_temperature","print_speed","fan_speed"]
+
 DEBUG = True
 VERSION = "1.0.0"
