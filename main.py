4111336fbcf958e9c6fc2ff14368e9f1ecaeb196
luunguyentankhai <tankhai15406@gmail.com>
2026-05-01 22:05:10 +0700

    add comment

--- main.py
+++ main.py
@@ -31,29 +31,35 @@ class Main:
 def main():
     Pulldata()
     xstk = Main()
-    # xstk.RP.Read_Data()
-    # print("\n---------------------------------\n")
-    # xstk.RP.check_missing_data()
-    # print("\n---------------------------------\n")
+    xstk.RP.Read_Data() #  gọi hàm để đọc dữ liệu từ file Read_Process
+    print("\n---------------------------------\n")
+    # sau đó chúng ta gọi hàm check_missing_data để kiểm thử rằng dữ liệu có bị NULL không
+    xstk.RP.check_missing_data()
+    xstk.ST.describe_numberical_data()
+    xstk.ST.describe_categorical_data()
+    print("\n---------------------------------\n")
     eda, preprocessing = xstk.RP.data_preprocessing()
     # print(eda)
     # print("\n---------------------------------\n")
     # print(preprocessing)
     # print("\n---------------------------------\n")
-    # xstk.MX.Matrix_corr(eda).plt_correlation_matrix()
-    # xstk.SP.Scatter(eda).plt_Scatter_plot()
-    # xstk.BX.Box(eda).plt_Box_plot()
-    # xstk.HS.Histogram(eda).plt_Histogram()
+
+    #khu vực chuyền biến eda để vẽ các đồ thị 
+    xstk.MX.Matrix_corr(eda).plt_correlation_matrix()
+    xstk.SP.Scatter(eda).plt_Scatter_plot()
+    xstk.BX.Box(eda).plt_Box_plot()
+    xstk.HS.Histogram(eda).plt_Histogram()
+    
 
     cols_to_remove = ['material_ABS', 'fan_speed']
     preprocessing = preprocessing.drop(columns=cols_to_remove, errors='ignore')
 
     fit_models, useless = xstk.OLS.MultipleRegression(preprocessing).fit_models()
     
-    # diagnostics_plt = xstk.MD.ModelDiagnostic(fit_models)
+    diagnostics_plt = xstk.MD.ModelDiagnostic(fit_models)
 
-    # diagnostics_plt.plt_scatter_residuals()
-    # diagnostics_plt.plt_QQ_normality()
+    diagnostics_plt.plt_scatter_residuals()
+    diagnostics_plt.plt_QQ_normality()
     for i in config.dependent_vars:
         xstk.SD.StatisticalDiagnostics(fit_models,i).Test_run()
         xstk.VIF.VIFChecker(preprocessing,useless,i).Check()
