0a07d075c48ffe956601d1121b465d6a291a137c
luunguyentankhai <tankhai15406@gmail.com>
2026-05-01 22:05:20 +0700

    new color

--- scripts/Diagram/Box_plot.py
+++ scripts/Diagram/Box_plot.py
@@ -11,14 +11,14 @@ class Box:
     def plt_Box_plot(self):
         for var in config.dependent_vars:
             plt.figure(figsize=(8,5))
-            sns.boxplot(x='material', y=var, data=self.Data)
+            sns.boxplot(x='material', y=var, data=self.Data, palette=["red", "pink"])
             plt.title(f"BoxPlot {var} theo Material")
             file_name = config.Assets / f"BoxPlot_{var}_theo_Material.png"
             plt.savefig(file_name,dpi=300,bbox_inches='tight')
         
         for var in config.dependent_vars:
             plt.figure(figsize=(8,5))
-            sns.boxplot(x='infill_pattern', y=var, data=self.Data)
+            sns.boxplot(x='infill_pattern', y=var, data=self.Data, palette=["red", "pink"])
             plt.title(f"BoxPlot {var} theo Infill_Pattern")
             file_name = config.Assets / f"BoxPlot_{var}_theo_Infill_Pattern.png"
             plt.savefig(file_name,dpi=300,bbox_inches='tight')
